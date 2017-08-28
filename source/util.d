5ad3a350cf617879a42170f9b067ea6b0c36e279
Kaiser <dogjaw2233@protonmail.com>
2017-08-27 22:13:18 -0400

    Added proper versioning.

--- source/util.d
+++ source/util.d
@@ -1,7 +1,16 @@
 module markov.util;
 import markov.core;
 import std.stdio;
-
+/**
+   This function steps through a markov chain set and returns the selected values in an array.
+   license: BSD 3-clause license
+   authors: John Whitley, dogjaw2233@protonmail.com
+   Date: August 27, 2017
+   Version: 0
+   Params:
+     start = The starting node.
+     iterator = Amount of times to step through.
+*/
 auto stepChain(T)(MarkovNode!T *start, long iterator) {
   T[] rarr;
   int i;
